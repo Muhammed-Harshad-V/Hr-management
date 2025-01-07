af80cca70d904f7cdf0b8b67dab8b41c66f170f9
Meagamer <irshadiru472@gmail.com>
2025-01-07 16:51:59 +0530

    user auth initail commit

---
+++ Authentication Service/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+// Middleware
+app.use(bodyParser.json());
+// app.use('/')
+// app.use('/')
+
+// MongoDB connection
+const dbURI = process.env.MURL;
+mongoose.connect(dbURI)
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
+
+// Basic route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
