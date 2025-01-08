3e4c8073abb016988c82e2c6e151ffadf95c56ef
Meagamer <irshadiru472@gmail.com>
2025-01-08 15:02:37 +0530

    gateway change

--- backend/gateway/app.js
+++ backend/gateway/app.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.use('/employeeService' , http('http://localhost:3001'));
 app.use('/userService' , http('http://localhost:3002'));
+app.use('/attendanceService' , http('http://localhost:3003'));
+app.use('/payrollService' , http('http://localhost:3004'));
 
 app.listen(3000, () => {
     console.log('gateway service is running on 3000')
