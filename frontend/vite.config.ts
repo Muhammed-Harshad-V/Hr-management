854473428658ad6ff862e6c4fbf6b36c95337ed1
Muhammed-Harshad-V <irshadiru472@gmail.com>
2025-02-02 09:47:03 +0530

    fix 2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base:"/dashboard",
+  base: "/dashboard",  // Ensuring the app is served from the '/dashboard' route
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src', // This defines '@' as the 'src' directory
+      '@': '/src',  // This defines '@' as the 'src' directory
     },
+  },
+  build: {
+    chunkSizeWarningLimit: 2000,  // Increase the chunk size limit to suppress the warning (e.g., 2MB)
+
   },
 });
